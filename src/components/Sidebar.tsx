07fb3ca134f5177b211a3446fe2ae6c57bbd2e60
Mohamad Mouaz Al Midani <m.m.almidani@gmail.com>
2025-10-30 09:55:26 +0400

    solve re-render Issue of the sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react'
+import { FC, useMemo, useState } from 'react'
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import { navItems } from '../constants/NavItems'
 import { useRouter } from 'expo-router'
@@ -17,72 +17,89 @@ const Sidebar: FC<{fullName: string, email: string}> = ({fullName, email}) => {
     const [hoveredUrl, setHoveredUrl] = useState<string | null>(null) 
 
     const handleNavigation = (path: string) => router.navigate(path)
-
+    console.log("--> re-render")
     const sidebarWidth = isLg ? "20%" : 80 
     const logoMargin = isLg ? 20 : 10 
 
-    const isHovered = (url: string) => url === hoveredUrl || url == "/"
+    const containerStyle = useMemo(() => [
+        style.container, 
+        { width: sidebarWidth }, 
+        isMd ? {paddingHorizontal: 5} : {paddingHorizontal: 30}
+    ], [sidebarWidth, isMd])
+
+    const logoStyle = useMemo(() => ({ 
+        margin: logoMargin, 
+        width: isLg ? 210 : 70, 
+        height: 70 
+    }), [logoMargin, isLg])
+
+    const userProfileStyle = useMemo(() => [
+        style.userProfile, 
+        !is2xl ? {justifyContent: 'center', height: 71, width: 71} : {}
+    ], [is2xl])
 
     return (
-        <View style={[style.container, { width: sidebarWidth }, isMd ? {paddingHorizontal: 5} : {paddingHorizontal: 30}]}>
+        <View style={containerStyle}>
             <Image
                 source={isLg ? require("../../assets/logo_dark.png") : require("../../assets/favicon.png")}
-                style={{ margin: logoMargin, width: isLg ? 210 : 70, height: 70 }}
+                style={logoStyle}
                 resizeMode='contain'
             />
             
             <View>
-                {navItems.map((nav: NavItem) => (
-                    <TouchableOpacity
-                        key={nav.url}
-                        onPress={() => handleNavigation(nav.url)}
-                        onPressIn={() => setHoveredUrl(nav.url)} 
-                        onPressOut={() => setHoveredUrl(null)}
-                        style={[
-                            style.navButton,
-                            {borderRadius: isLg ? 30 : 10},
-                            isHovered(nav.url) && { backgroundColor: Colors.secondary }
-                        ]}
-                    >
-                        <View style={[style.navItemContent, { 
-                            justifyContent: is2xl ? 'flex-start' : 'center',
-                            marginLeft: is2xl ? 30 : -10,
-                            width: 'auto',
-                        }]}>
-                            {/* Icon - dynamically change tintColor */}
-                            <Image
-                                source={nav.icon}
-                                style={[
-                                    style.navIcon,
-                                    {
-                                        tintColor: isHovered(nav.url) ? Colors.white : Colors.platinum 
-                                    }
-                                ]}
-                            />
-                            {/* Text (Desktop only) */}
-                            {isLg && (
-                                <Text style={[style.navText, {color: isHovered(nav.url) ? "white" : "black"}]}>{nav.name}</Text>
-                            )}
-                        </View>
-                    </TouchableOpacity>
-                ))}
+                {navItems.map((nav: NavItem) => {
+                    const isSelected = nav.url.trim() === hoveredUrl?.trim()
+                    
+                    return (
+                        <TouchableOpacity
+                            key={nav.url}
+                            onPress={() => handleNavigation(nav.url)}
+                            onPressIn={() => setHoveredUrl(nav.url)} 
+                            onPressOut={() => setHoveredUrl(null)}
+                            style={[
+                                style.navButton,
+                                {borderRadius: isLg ? 30 : 10},
+                                isSelected && style.navButtonSelected
+                            ]}
+                        >
+                            <View style={[style.navItemContent, { 
+                                justifyContent: is2xl ? 'flex-start' : 'center',
+                                marginLeft: is2xl ? 30 : -10,
+                                width: 'auto',
+                            }]}>
+                                <Image
+                                    source={nav.icon}
+                                    style={[
+                                        style.navIcon,
+                                        {
+                                            tintColor: isSelected ? Colors.white : Colors.platinum 
+                                        }
+                                    ]}
+                                />
+                                {isLg && (
+                                    <Text style={[style.navText, {color: isSelected ? "white" : "black"}]}>{nav.name}</Text>
+                                )}
+                            </View>
+                        </TouchableOpacity>
+                    )
+                })}
             </View>
             {is2xl && (
                 <View style={style.navStorageImage}>
                     <Image
-                        style={{width: 250, height: 250, marginTop: -70}} 
+                        style={style.storageImageStyle} 
                         source={require("../../assets/fileSearch.png")}
                     />
                 </View>
             )}
-            <View style={[style.userProfile, !is2xl? {justifyContent: 'center', height: 71, width: 71} : {}]}>
+            <View style={userProfileStyle}>
                 <Image
-                    style={{width: 60, height: 60, borderRadius: "100%"}} 
+                    style={style.profileImage} 
                     source={"https://www.shutterstock.com/image-vector/vector-bright-portrait-beautiful-brunette-600nw-2452267975.jpg"}
                 />
                 {is2xl && (
-                    <View style={{display: "flex", gap: 2}}>
-                        <Text style={{fontWeight: "700"}}>{fullName}</Text>
+                    <View style={style.profileInfo}>
+                        <Text style={style.profileName}>{fullName}</Text>
                         <Text>{email}</Text>
                     </View>
                 )}
@@ -111,6 +128,9 @@ const style = StyleSheet.create({
         marginHorizontal: 10,
         width: "100%"
     },
+    navButtonSelected: {
+        backgroundColor: Colors.secondary
+    },
     navItemContent: {
         display: "flex", 
         flexDirection: 'row', 
@@ -136,6 +156,23 @@ const style = StyleSheet.create({
         borderRadius: 20, 
         display: "flex", 
         alignItems: "center"
+    },
+    storageImageStyle: {
+        width: 250, 
+        height: 250, 
+        marginTop: -70
+    },
+    profileImage: {
+        width: 60, 
+        height: 60, 
+        borderRadius: "100%"
+    },
+    profileInfo: {
+        display: "flex", 
+        gap: 2
+    },
+    profileName: {
+        fontWeight: "700"
     },
     userProfile: {
         display: "flex", 
